54138635b5196dd304a42b9df3199f65de05cc6b
gustavo_sk@live.com <download>
2016-03-03 04:00:09 +0000

    script para inserir usuario: artmanager senha: artmanager

---
+++ scripts_sql/sprint_00/create_user_default.sql
@@ -0,0 +1,3 @@
+-- usuario: artmanager
+-- senha: artmanager'
+insert into tb_usuario(ds_nome, ds_usuario, ds_senha, nr_perfil) values ('ArtManager', 'artmanager', 'e3e4a815ee8645fcd7c1f9ae2407d803', 1);
