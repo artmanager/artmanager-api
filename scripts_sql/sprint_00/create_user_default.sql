31827a6000cd4f149491d937cbfb7bdbf807c7e8
Icaro <icbichir1@gmail.com>
2016-03-10 13:10:56 -0300

    corrections on sql script

--- scripts_sql/sprint_00/create_user_default.sql
+++ scripts_sql/sprint_00/create_user_default.sql
@@ -1,3 +1,3 @@
 -- usuario: artmanager
 -- senha: artmanager'
-insert into tb_usuario(ds_nome, ds_usuario, ds_senha, nr_perfil) values ('ArtManager', 'artmanager', 'e3e4a815ee8645fcd7c1f9ae2407d803', 1);
+insert into tb_user(ds_name, ds_user, ds_password, nr_profile) values ('ArtManager', 'artmanager', 'e3e4a815ee8645fcd7c1f9ae2407d803', 1);
