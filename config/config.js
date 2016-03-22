ec28d775e795449997f3fc3f87389fc94fad6a14
Icaro <icbichir1@gmail.com>
2016-03-22 10:45:37 -0300

    correcting the directory path

--- config/config.js
+++ config/config.js
@@ -12,6 +12,6 @@ module.exports = {
 		"url" : "http://localhost:3000"
 	},
 	"common" : {
-		"fileCommon" : "../../../artmanager-common/common.js"
+		"fileCommon" : "../../artmanager-common/common.js"
 	}
 }
